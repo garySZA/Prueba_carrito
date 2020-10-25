ecb8e57499922a1446024a2da1708fe0ba855b80
garySZA <garylvlsoliz@gmail.com>
2020-10-24 20:36:40 -0400

    primer commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+import "./style.css";
+import ContenedorCarrito from './ContenidoCarrito'
+
+
+class App extends React.Component {
+  render(){
+    return(
+      <ContenedorCarrito />
+    );
+  }
+}
+
+
+export default App;
